37034858d2b49ae043908cbcd538e7c327fd299a
Rafael Souza <rafaeljosegomes25@gmail.com>
2023-07-09 15:21:14 -0300

    set initial calculator styles

--- src/components/Calculadora.tsx
+++ src/components/Calculadora.tsx
@@ -1,12 +1,17 @@
 import { Button } from './Button'
 import { Display } from './Display'
+import styles from './Calculadora.module.css'
 
 export function Calculadora() {
     return (
-        <div>
-            <h1>Calculadora</h1>
-            <Button />
-            <Display />
+        <div className={styles.container}>
+            <div className={styles.content}>
+                <main className={styles.main}>
+                    <h1>Calculadora</h1>
+                    <Button />
+                    <Display />
+                </main>
+            </div>
         </div>
     )
 }
