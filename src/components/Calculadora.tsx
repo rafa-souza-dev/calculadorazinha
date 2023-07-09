e2b67c93411ff3bc7d52c83c875436c93b81d86c
Rafael Souza <rafaeljosegomes25@gmail.com>
2023-07-09 15:01:29 -0300

    feat: created main components

---
+++ src/components/Calculadora.tsx
@@ -0,0 +1,12 @@
+import { Button } from './Button'
+import { Display } from './Display'
+
+export function Calculadora() {
+    return (
+        <div>
+            <h1>Calculadora</h1>
+            <Button />
+            <Display />
+        </div>
+    )
+}
---
+++ src/components/Display.tsx
@@ -0,0 +1,5 @@
+export function Display() {
+    return (
+        <h1>Display</h1>
+    )
+}
