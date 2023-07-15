77a082c79a02df99320a95b88307794401ef6b6c
Rafael Souza <rafael.jose@ferreri.co>
2023-07-14 21:55:04 -0300

    created Button component

---
+++ src/@types/types.d.ts
@@ -0,0 +1,5 @@
+export type variant =
+    | 'black'
+    | 'brass'
+    | 'orange'
+    | 'silver'
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,5 +1,40 @@
-export function Button() {
+import { variant } from '../@types/types';
+import styles from './Button.module.css'
+
+interface ButtonProps {
+    slug: string;
+    handleFunction: () => void;
+    variant: variant
+}
+
+export function Button({
+    handleFunction,
+    slug,
+    variant
+}: ButtonProps) {
+    function getVariant(variant: variant) {
+        switch (variant) {
+            case 'black':
+                return 'variantBlack'
+            case 'brass':
+                return 'variantBrass'
+            case 'silver':
+                return 'variantSilver'
+            case 'orange':
+                return 'variantOrange'
+        }   
+    }
+
+    const variantClass = getVariant(variant)
+
+    console.log(styles[variantClass])
+
     return (
-        <h1>Button</h1>
+        <button
+            className={styles.button + " " + styles[variantClass]} 
+            onClick={handleFunction}
+        >
+            <span>{slug}</span>
+        </button>
     )
 }
